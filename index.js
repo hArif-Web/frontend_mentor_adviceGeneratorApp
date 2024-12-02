e1ccb595c2f2a962e959089ec1b670c62525428f
hArif-Web <156829045+hArif-Web@users.noreply.github.com>
2024-12-02 17:41:38 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+/*
+const generateAdvice = async () => {
+
+try {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    const advice = data.slip.advice;
+    const slipId = data.slip.id;   
+    
+    $('.adviceH1').empty();
+    $('.adviceP').empty();
+    $('.adviceH1').text(`Advice: #${slipId}`);
+    $('.adviceP').text(advice);
+
+
+} catch(error){
+    console.log(error);
+}    
+
+}
+
+
+
+$(document).ready(function () {
+    generateAdvice();
+    $('#adviceGenerator').on('click',generateAdvice);
+});
+*/
